49c777bde40b5f5c79fac57705b61c669eabdadb
Antonio Garcia <antonio.garciac@externos.correo.gob.es>
2023-04-02 17:13:53 +0200

    UI Simplified

--- lib/ui/feature/invoice_list_page/widgets/order_button.dart
+++ lib/ui/feature/invoice_list_page/widgets/order_button.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc_freezed_example/ui/model/filter_order.dart';
 import 'package:flutter_bloc_freezed_example/ui/model/invoice_order_state.dart';
 
 class SortChipsRow extends StatelessWidget {
@@ -12,57 +11,24 @@ class SortChipsRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: List<Widget>.generate(state.listInvoiceOrderState.length,
-          (int index) {
-        final isArrowUp =
-            state.listInvoiceOrderState[index].filterOrder.isAsc();
-        bool isSelected = state.listInvoiceOrderState[index].selected;
-        final textColor = isSelected ? Colors.white : Colors.black;
-        var backgroundColor = isSelected ? Colors.blue : Colors.grey.shade300;
-        final icon = isArrowUp ? Icons.arrow_drop_up : Icons.arrow_drop_down;
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
-          child: Container(
-            width: 100,
-            decoration: BoxDecoration(
-              color: backgroundColor,
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            child: GestureDetector(
-              onTap: () {
-                if (state.listInvoiceOrderState[index].selected) {
-                  // Toggle sort direction if the same chip is tapped again
-                  isArrowUp
-                      ? const FilterOrder.orderDesc()
-                      : const FilterOrder.orderAsc();
-                  // Call onChipSelected callback with the selected index and sort direction
-                  onChipSelected(state.toOppositeDirectionOnIndex(index));
-                } else {
-                  onChipSelected(state.updatedSelected(index));
-                }
-              },
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    state.listInvoiceOrderState[index].name,
-                    style: TextStyle(
-                      color: textColor,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Icon(
-                    icon,
-                    color: textColor,
-                  ),
-                ],
-              ),
-            ),
-          ),
-        );
-      }),
-    );
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.blueAccent, // foreground
+        ),
+        onPressed: () => DoNothingAction(),
+        child: const Text('Date'),
+      ),
+      const SizedBox(width: 16),
+      ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.blueAccent, // foreground
+        ),
+        onPressed: () => DoNothingAction(),
+        child: const Text('Number'),
+      )
+    ]);
   }
 }
